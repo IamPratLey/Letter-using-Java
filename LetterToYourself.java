03106a510e9c091a78a516728ee76153a0630355
Prathmesh <rafalemultirole94@gmail.com>
2022-07-01 21:00:43 +0530

    Uploading letter created in Java

---
+++ LetterToYourself.java
@@ -0,0 +1,25 @@
+public class LetterToYourself
+{
+
+       public static void main ( String[] args )
+       {
+        System.out.println("Letter created using Java language");
+             System.out.println("+--------------------------------------------------------------------------+");
+             System.out.println("|                                                                     #### |");
+             System.out.println("|                                                                     #### |");
+             System.out.println("|                                                                     #### |");
+             System.out.println("|                                                                          |");
+             System.out.println("|                                                                          |");
+             System.out.println("|                                     Heckter Shaw                         |");
+             System.out.println("|                                     9 Death Valley                       |");
+             System.out.println("|                                     Redmond, WA 98104                    |");
+             System.out.println("|                                                                          |");
+             System.out.println("|                                                                          |");
+             
+             System.out.println("+--------------------------------------------------------------------------+");
+
+
+
+
+       }
+}
